c7f909b4f3c9ecb01439ffced3c04908c2ae9082
rdfenix <rhoodsk8erboy@gmail.com>
2021-04-21 21:36:45 -0300

    creating podcastr

---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import styles from "../styles/app.module.scss";
+import "../styles/globals.scss";
+
+import { Header } from "../components/Header";
+import { Player } from "../components/Player";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
+    </div>
+  );
+}
+
+export default MyApp;
